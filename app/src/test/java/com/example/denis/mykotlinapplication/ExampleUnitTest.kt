1c6005d7e41e6bb9ba58d0c8db773b11fe6de952
Denis Timofeev <nakolenke@gmail.com>
2017-07-09 14:56:25 +0300

    Create project

---
+++ app/src/test/java/com/example/denis/mykotlinapplication/ExampleUnitTest.kt
@@ -0,0 +1,22 @@
+package com.example.denis.mykotlinapplication
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun kotlin_test() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
